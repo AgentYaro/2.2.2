2c03b3a1b2d5e136fec30d344b17719fa1cb9f89
AgentYaro <agent.yaro@gmail.com>
2022-06-29 11:45:41 +0300

    Task fixed

--- src/main/java/Service/CarServiceImp.java
+++ src/main/java/Service/CarServiceImp.java
@@ -4,20 +4,26 @@ import model.Car;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 @Component
-public class CarServiceImp implements CarService{
+public class CarServiceImp implements CarService {
+    private final List<Car> cars = new ArrayList<>(Arrays.asList(
+            new Car("Model1", "Colour1", 1),
+            new Car("Model2", "Colour2", 2),
+            new Car("Model3", "Colour3", 3),
+            new Car("Model4", "Colour4", 4),
+            new Car("Model5", "Colour5", 5)));
+
+
     @Override
-    public List<Car> numberOfCars(int number){
-        List<Car>cars =new ArrayList<Car>();
-        cars.add(new Car("Model1", "Colour1", 1));
-        cars.add(new Car("Model2", "Colour2", 2));
-        cars.add(new Car("Model3", "Colour3", 3));
-        cars.add(new Car("Model4", "Colour4", 4));
-        cars.add(new Car("Model5", "Colour5", 5));
-        if (number > cars.size()||number<=0) {
+    public List<Car> numberOfCars(int number) {
+
+        if (number > cars.size() || number <= 0) {
             number = cars.size();
         }
-        return cars.subList(0,number);
+
+        return cars.subList(0, number);
     }
 }
